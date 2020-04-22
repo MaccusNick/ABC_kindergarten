1a18f7e633d0df53fe88ae7cf7ebf0b7a0d75eba
MaccusNick <maccus0329@gmail.com>
2020-04-23 01:19:19 +1200

    new db location with basedir

--- abckindergarten/settings.py
+++ abckindergarten/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'abckindergarten.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('static/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'static/db.sqlite3'),
     }
 }
 
