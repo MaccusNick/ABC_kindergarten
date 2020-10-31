36e6e1087693261f2706532b08a9ad35f86c49b1
MaccusNick <maccus0329@gmail.com>
2020-10-31 16:16:28 +1300

    'upload'

--- abckindergarten/settings.py
+++ abckindergarten/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'abckindergarten.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+        'DIRS': [
                  os.path.join(BASE_DIR, 'dist')
                  ]
         ,
